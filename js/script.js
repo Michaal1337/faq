20f33e1b386e71d4421a50733ea9fbe4f652713d
Michal <michalm1337@gmail.com>
2023-07-10 14:53:00 +0200

    final version

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
-// const title = document.querySelectorAll('.focus')
-// const answer = document.querySelectorAll('.hidden')
-// const img = document.querySelectorAll('img')
-// const container = document.querySelector('.container__text')
 const faq = document.querySelectorAll('.container-faq')
 
 faq.forEach(item => item.addEventListener('click', () => {
 	if(item.classList.contains('active')) {
 		item.classList.remove('active')
 	} else {
-		faq.forEach(item2 => item2.classList.remove('active'))
+		// faq.forEach(item2 => item2.classList.remove('active'))
 		item.classList.toggle('active')
 	}
 	
